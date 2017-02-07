9d563503deb76c591e87831ca87878d8a7a520d5
Phong Cao <phong.caothanh@apps-cyclone.com>
2017-02-07 09:31:37 +0700

    Add layout subview

---
+++ CPGradientImage/Classes/CPGradientDirection.swift
@@ -0,0 +1,9 @@
+//
+//  CPGradientDirection.swift
+//  Pods
+//
+//  Created by Phong Cao on 12/22/16.
+//
+//
+
+import Foundation
---
+++ CPGradientImage/Classes/CPGradientImage.swift
@@ -0,0 +1,9 @@
+//
+//  CPGradientImage.swift
+//  Pods
+//
+//  Created by Phong Cao on 12/22/16.
+//
+//
+
+import Foundation
